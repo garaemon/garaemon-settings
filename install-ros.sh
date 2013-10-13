068351312cd544dc0b772c0e119d1a2cb441a766
Ryohei Ueda <garaemon@ubuntu.(none)>
2013-10-13 10:13:13 -0700

    install dependending packages to euslisp

--- install-ros.sh
+++ install-ros.sh
@@ -4,3 +4,5 @@
 wget 'https://sourceforge.net/p/jsk-ros-pkg/code/HEAD/tree/trunk/jsk.rosbuild?format=raw' -O /tmp/jsk.rosbuild
 yes | bash /tmp/jsk.rosbuild groovy
 sudo aptitude install ros-groovy-pr2-moveit-config openni_launch openni_camera
+source ~/ros/groovy/setup.zsh
+rosdep install euslisp
