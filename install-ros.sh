918da72a097d6e6cbd2994241dd4ae0493ba266a
Ryohei Ueda <garaemon@ubuntu.(none)>
2013-10-13 09:59:59 -0700

    adding script to install ros

---
+++ install-ros.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# install-ros.sh
+
+wget 'https://sourceforge.net/p/jsk-ros-pkg/code/HEAD/tree/trunk/jsk.rosbuild?format=raw' -O /tmp/jsk.rosbuild
+yes | bash /tmp/jsk.rosbuild groovy
+sudo aptitude install ros-groovy-pr2-moveit-config openni_launch openni_camera
