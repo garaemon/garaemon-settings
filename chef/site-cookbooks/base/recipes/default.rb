8990435ee32fcce1ada57ca6e1d1c0f26446326a
garaemon <garaemon@gmail.com>
2014-02-03 21:57:33 +0900

    adding ros to install

--- chef/site-cookbooks/base/recipes/default.rb
+++ chef/site-cookbooks/base/recipes/default.rb
@@ -13,17 +13,3 @@
   end
 end
 
-# adding ROS repository
-apt_repository 'ros-latest' do
-  uri 'http://packages.ros.org/ros/ubuntu'
-  distribution node['lsb']['codename']
-  key 'http://packages.ros.org/ros.key'
-  components   ['main']
-end
-
-# install ros package
-%w{ros-hydro-desktop-full ros-groovy-desktop-full}.each do |pkg|
-  package pkg do
-    action :install
-  end
-end
