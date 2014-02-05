4fa9e2c2a274d9b1b4ff93f4e1191f870c0afe28
garaemon <garaemon@gmail.com>
2014-02-05 18:27:32 +0900

    adding default attribute for ros-desktop

---
+++ chef/site-cookbooks/ros-desktop/attributes/default.rb
@@ -0,0 +1,2 @@
+default["ros-desktop"]["catkin_ws"] = "ros_catkin_ws"
+default["ros-desktop"]["rosbuild_ws"] = "ros"
--- chef/site-cookbooks/ros-desktop/recipes/default.rb
+++ chef/site-cookbooks/ros-desktop/recipes/default.rb
@@ -21,13 +21,69 @@ end
   end
 end
 
-# create catkin workspace
-# home_directory = nodes[:base_configuration][:home_dir]
-# user = nodes[:base_configuration][:user]
-# directory "#{home_directory}/hoge" do
-#   action :create
-#   owener user
-# end
+def create_dirs(dirs)
+  user = node["base_configuration"]["user"]
+  dirs.each do |dir|
+    directory dir do
+    action :create
+    owner user
+    end
+  end
+end
 
+# create catkin workspace
+
+def setup_catkin(distro)
+  catkin_ws = node["ros-desktop"]["catkin_ws"]
+  home_directory = node["base_configuration"]["home_dir"]
+  directories = ["#{home_directory}/#{catkin_ws}",
+                 "#{home_directory}/#{catkin_ws}/#{distro}",
+                 "#{home_directory}/#{catkin_ws}/#{distro}/src"]
+  create_dirs(directories)
+  
+  unless File.exists? "#{home_directory}/#{catkin_ws}/#{distro}/src/CMakeLists.txt" then
+    bash "catkin_init_workspace" do
+      user user
+      cwd "#{home_directory}/#{catkin_ws}/#{distro}/src"
+      code <<-EOH
+       source /opt/ros/#{distro}/setup.sh
+       catkin_init_workspace
+      EOH
+    end
+    bash "catkin_make" do
+      user user
+      cwd "#{home_directory}/#{catkin_ws}/#{distro}"
+      code <<-EOH
+       source /opt/ros/#{distro}/setup.sh
+       catkin_make
+      EOH
+    end
+  end
+end
+
+setup_catkin("hydro")
+setup_catkin("groovy")
 
 # create rosbuild workspace
+def setup_rosbuild(distro)
+  rosbuild_ws = node["ros-desktop"]["rosbuild_ws"]
+  catkin_ws = node["ros-desktop"]["catkin_ws"]
+  home_directory = node["base_configuration"]["home_dir"]
+  directories = ["#{home_directory}/#{rosbuild_ws}",
+                 "#{home_directory}/#{rosbuild_ws}/#{distro}"]
+  create_dirs(directories)
+  # run rosws init
+  unless File.exists? "#{home_directory}/#{rosbuild_ws}/#{distro}/.rosinstall" then
+    bash "rosws_init" do
+      user user
+      cwd "#{home_directory}/#{rosbuild_ws}/#{distro}"
+      code <<-EOH
+        source #{home_directory}/#{catkin_ws}/#{distro}/devel/setup.bash
+        rosws init . #{home_directory}/#{catkin_ws}/#{distro}/devel
+      EOH
+    end
+  end
+end
+
+setup_rosbuild("hydro")
+setup_rosbuild("groovy")
