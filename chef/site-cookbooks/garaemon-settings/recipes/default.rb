db16186220443e586549a76b03e1dd7988acc066
garaemon <garaemon@gmail.com>
2014-02-13 02:21:41 +0900

    adding font

--- chef/site-cookbooks/garaemon-settings/recipes/default.rb
+++ chef/site-cookbooks/garaemon-settings/recipes/default.rb
@@ -19,6 +19,7 @@ git_root_dir = node["garaemon-settings"]["git_root"]
 # packages
 %w{zsh aptitude git-core emacs vim tmux anthy-el ssh zsh curl htop
    python-pip
+   ttf-dejavu
    sqlite3 libgdbm-dev bison libffi-dev
    virtualbox}.each do |pkg|
   package pkg do
