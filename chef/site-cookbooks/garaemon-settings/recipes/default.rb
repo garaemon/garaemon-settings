e2787e925af68af2ef4d54676b7535903469758c
garaemon <garaemon@gmail.com>
2014-02-14 03:50:58 +0900

    adding datacounter fluentd plugin

--- chef/site-cookbooks/garaemon-settings/recipes/default.rb
+++ chef/site-cookbooks/garaemon-settings/recipes/default.rb
@@ -134,7 +134,7 @@ bash "install rvm" do
 end
 
 ruby_versions = node["garaemon-settings"]["ruby-versions"]
-gem_packages = %w{vagrant travis fluentd t fluent-plugin-dstat}
+gem_packages = %w{vagrant travis fluentd t fluent-plugin-dstat fluent-plugin-datacounter}
 ruby_versions.each do |version|
   bash "install ruby #{version}" do
     user user
