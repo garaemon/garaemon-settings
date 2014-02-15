34bf6c929f1eaf456185eaa9b43cd8e6f731aa1d
garaemon <garaemon@gmail.com>
2014-02-16 02:37:23 +0900

    adding tig

--- chef/site-cookbooks/garaemon-settings/recipes/default.rb
+++ chef/site-cookbooks/garaemon-settings/recipes/default.rb
@@ -18,7 +18,7 @@ git_root_dir = node["garaemon-settings"]["git_root"]
 
 # packages
 %w{zsh aptitude git-core emacs vim tmux anthy-el ssh zsh curl htop
-   python-pip
+   python-pip tig
    ttf-dejavu
    sqlite3 libgdbm-dev bison libffi-dev dstat
    virtualbox}.each do |pkg|
