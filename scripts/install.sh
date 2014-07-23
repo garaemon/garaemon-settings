7e7f833e9eff611f548874a55783382cf39c1826
Ryohei Ueda <garaemon@gmail.com>
2014-07-23 14:58:06 +0900

    install powerline

--- scripts/install.sh
+++ scripts/install.sh
@@ -122,6 +122,8 @@ nvm install v0.10.28
 redecho ">> [installing rvm]"
 if [ ! -e ~/.rvm ]; then
     curl -sSL https://get.rvm.io | bash -s stable
+# else
+#     rvm get stable
 fi
 source ~/.rvm/scripts/rvm
 
@@ -139,3 +141,14 @@ if [ ! -e ~/.emacs.d -o ! -L ~/.emacs.d ]; then
 fi
 
 # powerline
+redecho ">> [installing powerline]"
+pip install --user git+git://github.com/Lokaltog/powerline
+
+redecho ">> [installing powerline font]"
+mkdir -p ~/.fonts
+wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf -O ~/.fonts/PowerlineSymbols.otf
+fc-cache -vf ~/.fonts
+mkdir -p ~/.fonts.conf.d
+wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf -O ~/.fonts.conf.d/10-powerline-symbols.conf
+mkdir -p ~/.config
+ln -sf $GRPOG_DIR/garaemon-settings/resources/powerline ~/.config/powerline
