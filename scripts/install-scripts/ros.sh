62d86ad1f326e410c3d8f303ad7236e3c1a85ec4
Ryohei Ueda <garaemon@gmail.com>
2014-10-13 16:37:49 +0900

    run jsk script to instal lros

--- scripts/install-scripts/ros.sh
+++ scripts/install-scripts/ros.sh
@@ -10,3 +10,7 @@ mkdir -p $HOME/ros
 mkdir -p $HOME/ros/hydro/src
 mkdir -p $HOME/ros/groovy/src
 mkdir -p $HOME/ros/groovy.catkin/src
+
+# setup hydro by jsk script
+wget -q -O /tmp/jsk.rosbuild https://raw.github.com/jsk-ros-pkg/jsk_common/master/jsk.rosbuild
+bash /tmp/jsk.rosbuild hydro
