47e94b638d14d975c207acc0ae8a473ff52a806e
Ryohei Ueda <garaemon@gmail.com>
2014-10-13 14:05:40 +0900

    compile emacs

--- scripts/install-scripts/emacs.sh
+++ scripts/install-scripts/emacs.sh
@@ -4,6 +4,15 @@ expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 
 . $cwd/../lib.sh
 
+if [ ! -e $HOME/.local/emacs ]; then
+    cyanecho ">>>> [compiling emacs]"
+    cd $GPROG_DIR
+    ./autogen.sh
+    ./confiugre.sh --prefix $HOME/.local
+    make -j$(grep -c processor /proc/cpuinfo)
+    make install
+fi
+
 cyanecho ">>>> [linking .emacs]"
 if [ ! -e ~/.emacs -o ! -L ~/.emacs ]; then
     rm -rf ~/.emacs
