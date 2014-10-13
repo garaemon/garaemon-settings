3d0e52688accd9f8d97b5b362bc152c8f94e662f
Ryohei Ueda <garaemon@gmail.com>
2014-10-13 16:01:13 +0900

    update bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-set -e
-
-sudo apt-get install ssh
+cd /tmp
+rm -rf garaemon-settings
+git clone https://github.com/garaemon/garaemon-settings.git
+cd garaemon-settings/scripts
+./install.sh
