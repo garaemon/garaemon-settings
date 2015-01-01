4b1de4b7978925e37e9e3efe01a77bcfc411201d
Ryohei Ueda <garaemon@gmail.com>
2015-01-01 16:30:53 +0900

    Do not merge --no-ff when pull

--- scripts/install-scripts/git.sh
+++ scripts/install-scripts/git.sh
@@ -10,6 +10,9 @@ if [ ! -e /usr/share/git-core/templates/hooks/commit-msg ]; then
 fi
 cyanecho ">>>> [disabling git ff merge]"
 git config --global --add merge.ff false
+git config branch.master.rebase true
+git config --global pull.rebase true
+git config --global pull.rebase preserve
 
 cyanecho ">>>> [setting up git user.name]"
 git config --global user.name "Ryohei Ueda"
