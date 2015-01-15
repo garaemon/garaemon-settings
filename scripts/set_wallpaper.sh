07384146b4d9bdff6330f0bde8a4b6e2d23d02a8
Ryohei Ueda <garaemon@gmail.com>
2015-01-15 17:28:39 +0900

    Add username

--- scripts/set_wallpaper.sh
+++ scripts/set_wallpaper.sh
@@ -6,6 +6,6 @@ wget "http://www.freitag.ch/medias/sys_master/8797576593438/F41-Hawaii-Five-O-02
 # size=$(xrandr | grep '*+' | head -n 1 | awk '{print $1}')
 hostname=$(hostname)
 if [ ! -e ~/.local/wallpaper/wallpaper_$hostname.jpg ]; then
-    convert -font Helvetica -pointsize 100 -fill "#ffffff" -draw "text 2000,1600 $hostname" ~/.local/wallpaper/original.jpg ~/.local/wallpaper/wallpaper_$hostname.jpg
+    convert -font Helvetica -pointsize 100 -fill "#ffffff" -draw "text 1500,1650 $USER@$hostname" ~/.local/wallpaper/original.jpg ~/.local/wallpaper/wallpaper_$hostname.jpg
     gsettings set org.gnome.desktop.background draw-background false && gsettings set org.gnome.desktop.background picture-uri file:///$HOME/.local/wallpaper/wallpaper_$hostname.jpg && gsettings set org.gnome.desktop.background draw-background true
 fi
