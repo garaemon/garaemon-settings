6c3c3e82b7191852710d9c1fa8dd46d6fc1476da
Ryohei Ueda <garaemon@gmail.com>
2015-01-18 22:44:26 +0900

    install gem packages

--- scripts/install-scripts/ruby.sh
+++ scripts/install-scripts/ruby.sh
@@ -4,9 +4,12 @@ expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 
 . $cwd/../lib.sh
 
+PACKAGES="travis github-pages"
+RUBY_VERSION="ruby-2.2.0"
 cyanecho ">>>> [installing rvm]"
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
 if [ ! -e ~/.rvm ]; then
+    gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
     curl -sSL https://get.rvm.io | bash -s stable
 # else
 #     rvm get stable
@@ -14,4 +17,9 @@ fi
 source ~/.rvm/scripts/rvm
 
 cyanecho ">>>> [installing current ruby]"
-rvm install ruby-v2.2.0
+rvm install $RUBY_VERSION
+rvm use $RUBY_VERSION
+
+
+cyanecho ">>>> [installing $PACKAGES]"
+gem install $PACKAGES
