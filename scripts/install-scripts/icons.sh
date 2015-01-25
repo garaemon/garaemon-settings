f8a0cd6f23a335fe3e842145b3f9eb7643a55a1e
Ryohei Ueda <garaemon@gmail.com>
2015-01-25 13:49:58 +0900

    Add emacs icon

--- scripts/install-scripts/icons.sh
+++ scripts/install-scripts/icons.sh
@@ -58,5 +58,22 @@ Icon=gnome-panel-clock
 EOF
     chmod +x $HOME/.config/gnome-panel/launchers/TogglDesktop.desktop
 fi
+if [ ! -e "$HOME/.config/gnome-panel/launchers/emacs.desktop" ]; then
+    cyanecho ">>>> [generating emacs desktop]"
+    cat <<EOF > $HOME/.config/gnome-panel/launchers/emacs.desktop
+[Desktop Entry]
+Name=Emacs
+GenericName=Text Editor
+Comment=Edit text
+MimeType=text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;
+Exec=$HOME/.local/bin/emacs %F
+Icon=emacs
+Type=Application
+Terminal=false
+Categories=Development;TextEditor;
+StartupWMClass=Emacs
+EOF
+    chmod +x $HOME/.config/gnome-panel/launchers/emacs.desktop
+fi
 
 xdg-open $HOME/.config/gnome-panel/launchers
