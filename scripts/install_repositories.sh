f39f6bb0f5a5c3638f5810e8266eb9b3416f49af
Ryohei Ueda <garaemon@gmail.com>
2015-09-27 02:16:40 +0900

    Add npm-check

--- scripts/install_repositories.sh
+++ scripts/install_repositories.sh
@@ -3,7 +3,8 @@
 REPOS="https://github.com/rhysd/Shiba.git \
 git@github.com:garaemon/pltcli.git \
 git@github.com:garaemon/multi_crop.git \
-git@github.com:garaemon/simpler-dot.git"
+git@github.com:garaemon/simpler-dot.git \
+https://github.com/dylang/npm-check.git"
 
 for r in $REPOS
 do
