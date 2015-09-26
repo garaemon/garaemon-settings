f7eb7fd5cf84cb97c135523ffd0142197658b729
Ryohei Ueda <garaemon@gmail.com>
2015-09-27 01:53:47 +0900

    Install npm packages when setup

--- scripts/install-scripts/node.sh
+++ scripts/install-scripts/node.sh
@@ -3,7 +3,7 @@ cwd=`dirname "${0}"`
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 
 . $cwd/../lib.sh
-
+packages="shiba prettyjson"
 cyanecho ">>>> [installing nvm]"
 if [ ! -e ~/.nvm ]; then
     curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
@@ -11,3 +11,5 @@ fi
 source ~/.nvm/nvm.sh
 cyanecho ">>>> [install node.js v0.10.28]"
 nvm install v0.10.28
+
+npm install -g $packages
