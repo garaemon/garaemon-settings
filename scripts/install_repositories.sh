5a6c36cf16e46564fe31b0cc0d80b1e36d5ad3b9
Ryohei Ueda <garaemon@gmail.com>
2015-09-29 14:48:02 +0900

    Add tider-pdfimages

--- scripts/install_repositories.sh
+++ scripts/install_repositories.sh
@@ -4,7 +4,8 @@ REPOS="https://github.com/rhysd/Shiba.git \
 git@github.com:garaemon/pltcli.git \
 git@github.com:garaemon/multi_crop.git \
 git@github.com:garaemon/simpler-dot.git \
-https://github.com/dylang/npm-check.git"
+https://github.com/dylang/npm-check.git \
+https://github.com/nbeaver/tidier-pdfimages.git"
 
 for r in $REPOS
 do
