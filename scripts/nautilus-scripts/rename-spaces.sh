296945b2032f157a7e743dfdcf774f0b6e86c908
Ryohei Ueda <garaemon@gmail.com>
2015-09-30 12:34:14 +0900

    Support multiple files

--- scripts/nautilus-scripts/rename-spaces.sh
+++ scripts/nautilus-scripts/rename-spaces.sh
@@ -3,5 +3,10 @@
 # cp rename-spaces.sh ~/.local/share/nautilus/scripts/
 # mkdir -p ~/.gnome2/nautilus-scripts
 # cp rename-spaces.sh ~/.gnome2/nautilus-scripts
-mv -i "$*" "$(echo ${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS} | sed -e 's/\s\+/_/g')"
-exit0
+IFS="
+"
+for f in $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS
+do
+    mv -i "$f" "$(echo $f | sed -e 's/\s\+/_/g')"
+done
+exit
