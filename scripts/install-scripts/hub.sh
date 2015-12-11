0618d4625b9e17ae11edb00c1bbe0d67fb981d36
Ryohei Ueda <garaemon@gmail.com>
2015-12-11 15:50:45 +0900

    Add -q option to download hub

--- scripts/install-scripts/hub.sh
+++ scripts/install-scripts/hub.sh
@@ -6,7 +6,7 @@ expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 
 if [ ! -e ~/.local/bin/hub ]; then
     cyanecho ">>>> [installing hub binary]"
-    wget https://github.com/github/hub/releases/download/v2.2.0/hub-linux-amd64-2.2.0.tar.gz -O /tmp/hub.tar.gz
+    wget https://github.com/github/hub/releases/download/v2.2.0/hub-linux-amd64-2.2.0.tar.gz -O /tmp/hub.tar.gz -q
     cd /tmp
     tar xvzf hub.tar.gz
     mv hub-linux*/hub ~/.local/bin/hub
