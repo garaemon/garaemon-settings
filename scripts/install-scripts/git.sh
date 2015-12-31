7684100ed3ecb81d13b348a349c8677792bd7cfa
Ryohei Ueda <garaemon@gmail.com>
2015-12-31 21:17:29 +0900

    Compile the latest git

--- scripts/install-scripts/git.sh
+++ scripts/install-scripts/git.sh
@@ -4,15 +4,25 @@ expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 
 . $cwd/../lib.sh
 
-cyanecho ">>>> [installing git commit-msg]"
-if [ ! -e /usr/share/git-core/templates/hooks/commit-msg ]; then
-    runsudo cp -fv $GPROG_DIR/garaemon-settings/resources/git/commit-msg /usr/share/git-core/templates/hooks
+# cyanecho ">>>> [installing git commit-msg]"
+# if [ ! -e /usr/share/git-core/templates/hooks/commit-msg ]; then
+#     runsudo cp -fv $GPROG_DIR/garaemon-settings/resources/git/commit-msg /usr/share/git-core/templates/hooks
+# fi
+# cyanecho ">>>> [disabling git ff merge]"
+# git config --global --add merge.ff false
+# git config branch.master.rebase true
+# git config --global pull.rebase true
+# #git config --global pull.rebase preserve
+
+if [ ! -e $HOME/.local/bin/git ]; then
+    cyanecho ">>>> [cloning git]"
+    cd $GPROG_DIR
+    git clone https://github.com/git/git.git
+    cd git
+    ./configure --prefix=$HOME/.local
+    make all -j8
+    make install
 fi
-cyanecho ">>>> [disabling git ff merge]"
-git config --global --add merge.ff false
-git config branch.master.rebase true
-git config --global pull.rebase true
-#git config --global pull.rebase preserve
 
 cyanecho ">>>> [setting up git user.name]"
 git config --global user.name "Ryohei Ueda"
