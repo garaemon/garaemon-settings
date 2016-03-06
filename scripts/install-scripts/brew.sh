cbe6de9a2fe28eb7f475460e007fb69731f98fc2
Ryohei Ueda <garaemon@gmail.com>
2016-03-06 14:07:00 +0900

    Install unity

--- scripts/install-scripts/brew.sh
+++ scripts/install-scripts/brew.sh
@@ -20,4 +20,4 @@ brew linkapps emacs
 cyanecho ">>>> enabling cask"
 brew tap caskroom/cask
 
-brew cask install hyperswitch google-chrome kindle dropbox
+brew cask install hyperswitch google-chrome kindle dropbox unity
