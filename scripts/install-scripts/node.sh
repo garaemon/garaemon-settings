e27c88e9105c62bbaf2573725595344cbd62777e
Ryohei Ueda <garaemon@gmail.com>
2016-03-06 14:07:29 +0900

    Install gulp and yeoman

--- scripts/install-scripts/node.sh
+++ scripts/install-scripts/node.sh
@@ -3,14 +3,14 @@ cwd=`dirname "${0}"`
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 
 . $cwd/../lib.sh
-packages="shiba prettyjson npm-check"
+packages="shiba prettyjson npm-check yo bower grunt-cli gulp generator-electron"
 
 cyanecho ">>>> [installing nvm]"
 if [ ! -e ~/.nvm ]; then
     curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
 fi
 source ~/.nvm/nvm.sh
-cyanecho ">>>> [install node.js v0.10.28]"
-nvm install v0.10.28
+cyanecho ">>>> [install node.js v5.7.0]"
+nvm install v5.7.0
 
 npm install -g $packages
