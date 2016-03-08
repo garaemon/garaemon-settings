6ce94cf1e8de5305be646dc313b5bdf82f08a159
Ryohei Ueda <garaemon@gmail.com>
2016-03-08 14:38:48 +0900

    Add more message

--- scripts/install-scripts/node.sh
+++ scripts/install-scripts/node.sh
@@ -3,6 +3,7 @@ cwd=`dirname "${0}"`
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 
 . $cwd/../lib.sh
+target_node_version=v5.7.0
 packages="shiba prettyjson npm-check yo bower grunt-cli gulp generator-electron"
 
 cyanecho ">>>> [installing nvm]"
@@ -10,7 +11,7 @@ if [ ! -e ~/.nvm ]; then
     curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
 fi
 source ~/.nvm/nvm.sh
-cyanecho ">>>> [install node.js v5.7.0]"
-nvm install v5.7.0
-
+cyanecho ">>>> [install node.js ${target_node_version}]"
+nvm install ${target_node_version}
+cyanecho ">>>> [install npm packages]"
 npm install -g $packages
