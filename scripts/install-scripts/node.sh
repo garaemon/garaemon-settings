28772e3c61aa793d43383dcc895650496028f111
Ryohei Ueda <garaemon@gmail.com>
2016-11-20 13:28:15 +0900

    Fix coding style of node.sh to pass shellcheck

--- scripts/install-scripts/node.sh
+++ scripts/install-scripts/node.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-cwd=`dirname "${0}"`
-expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
+cwd=$(dirname "${0}")
+expr "${0}" : "/.*" > /dev/null || cwd=$(cd "${cwd}" && pwd)
 
-. $cwd/../lib.sh
+# shellcheck source=../lib.sh disable=SC1091
+source "${cwd}"/../lib.sh
+
 target_node_version=v6.9.0
 packages="shiba prettyjson npm-check yo bower grunt-cli gulp generator-electron babel-cli"
 
@@ -10,8 +12,11 @@ cyanecho ">>>> [installing nvm]"
 if [ ! -e ~/.nvm ]; then
     curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
 fi
+
+# shellcheck source=.~/.nvm/nvm.sh disable=SC1091
 source ~/.nvm/nvm.sh
 cyanecho ">>>> [install node.js ${target_node_version}]"
 nvm install ${target_node_version}
+
 cyanecho ">>>> [install npm packages]"
-npm install -g $packages
+npm install -g "${packages}"
