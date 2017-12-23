9245211cf258db35955459ed89f023f448d8bbb3
Ryohei Ueda <garaemon@gmail.com>
2017-12-23 14:28:17 +0900

    Add iterm2

--- scripts/install-scripts/brew.sh
+++ scripts/install-scripts/brew.sh
@@ -27,4 +27,4 @@ if [ ! $(sw_vers -productVersion | grep 10.12)] ; then
     brew cask install karabiner hyperswitch
 fi
 brew cask install google-chrome kindle dropbox vlc skype google-japanese-ime libreoffice \
-     meshlab emacs
+     meshlab emacs iterm2
