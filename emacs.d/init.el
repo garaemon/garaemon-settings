bbead4e06cdf94e632217e1c3046cae9947dee69
Ryohei Ueda <garaemon@gmail.com>
2025-03-12 12:19:23 -0700

    Reuse ssh connection for tramp

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1863,6 +1863,10 @@ FINISH-FUNC - callback which will be printed after main function finished"
  '(custom-safe-themes
    '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa"
      default))
+  '(tramp-ssh-controlmaster-options
+    (concat
+     "-o ControlPath=/tmp/ssh-ControlPath-%%r@%%h:%%p "
+     "-o ControlMaster=auto -o ControlPersist=yes"))
  '(package-selected-packages
    '(auto-package-update avy-migemo base16-theme browse-at-remote capf clang-format cmake-mode company company-statistics consult consult-ag corfu counsel counsel-projectile cquery dictionary diff-hl embark embark-consult esup exec-path-from-shell flycheck-eglot forge gcmh git-gutter gptel imenu-list imenus ivy-posframe ivy-prescient lsp-python-ms lsp-treemacs magit-gh-pulls marginalia minimap modern-cpp-font-lock multi-vterm nlinum orderless org-download origami prettier-js py-yapf slack solarized-theme switch-buffer-functions systemd total-lines transpose-frame typescript udev-mode use-package vertico vterm vterm-toggle)))
 (put 'upcase-region 'disabled nil)
