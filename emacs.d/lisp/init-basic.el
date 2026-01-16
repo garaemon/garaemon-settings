2c4185251db1d3c92d9501b73348dbaf89e7de10
Ryohei Ueda <garaemon@gmail.com>
2026-01-15 22:53:18 -0800

    Enable auto revert mode

--- emacs.d/lisp/init-basic.el
+++ emacs.d/lisp/init-basic.el
@@ -130,5 +130,10 @@
 ;;; Highlight indentation mode hook
 (add-hook 'highlight-indentation-mode (lambda () (highlight-indentation-mode -1)))
 
+;; Read buffer automatically if an external process modifies it
+(use-package autorevert
+  :ensure nil
+  :init (global-auto-revert-mode))
+
 (provide 'init-basic)
 ;;; init-basic.el ends here
